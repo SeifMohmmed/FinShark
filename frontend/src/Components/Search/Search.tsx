7e6c72d4d1129dd76d31e511d851f9258e2417ea
Seif Mohamed <saif.eissa7797@gmail.com>
2026-08-11 23:51:34 +0300

    Add Events

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,22 +1,26 @@
-import { useState } from "react";
+import { useState, type ChangeEvent, type SyntheticEvent } from "react";
 
 type Props = {};
 
 const Search: React.FC<Props> = (props: Props) => {
   const [search, setSearch] = useState<string>("");
 
-  const onClick = (e: any) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
     console.log(e);
   };
 
+  const onClick = (e: SyntheticEvent) => {
+    console.log(e);
+  };
   return (
     <div>
       <input
         value={search}
-        onChange={onClick}
+        onChange={(e) => handleChange(e)}
         placeholder="Search for a stock..."
-      />
+      ></input>
+      <button onClick={(e) => onClick(e)}>Search</button>
     </div>
   );
 };
