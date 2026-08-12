47b73fa2963ce9a53c8815e04b6032f9916e90b7
Seif Mohamed <saif.eissa7797@gmail.com>
2026-08-12 19:05:52 +0300

    Consume First Endpoint

---
+++ frontend/src/api.tsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+import type { CompanySearch } from "./company";
+
+interface SearchResponse {
+  data: CompanySearch[];
+}
+
+export const searchCompanies = async (query: string) => {
+  try {
+    const data = await axios.get<SearchResponse>(
+      `https://financialmodelingprep.com/stable/search-symbol?query=${query}&apikey=${import.meta.env.VITE_API_KEY}`,
+    );
+
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("error message: ", error.message);
+      return error.message;
+    } else {
+      console.log("unexpected error: ", error);
+      return "An expected error has occurred.";
+    }
+  }
+};
