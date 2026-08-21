9e347629b18e5bf912c64fa677ed8a514b2235c7
Seif Mohamed <saif.eissa7797@gmail.com>
2026-08-21 16:50:06 +0300

    Add Tailwind

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -15,14 +15,16 @@ const Card: React.FC<Props> = ({
   onPortfolioCreate,
 }: Props) => {
   return (
-    <div key={id} className="card">
-      <div className="details">
-        <h2>
-          {searchResults.name} ({searchResults.symbol})
-        </h2>
-        <p>${searchResults.currency}</p>
-      </div>
-      <p className="info">
+    <div
+      className="flex flex-col items-center justify-between w-full p-6 bg-slate-100 rounded-lg md:flex-row"
+      key={id}
+      id={id}
+    >
+      <h2 className="font-bold text-center text-black md:text-left">
+        {searchResults.name} ({searchResults.symbol})
+      </h2>
+      <p className="text-black">{searchResults.currency}</p>
+      <p className="font-bold text-black">
         {searchResults.exchangeShortName} - {searchResults.stockExchange}
       </p>
       <AddPortfolio
