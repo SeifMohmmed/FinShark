03a20f8186bb1c3793a37286cdbe52abf8732cb3
Seif Mohamed <saif.eissa7797@gmail.com>
2026-08-28 18:41:19 +0300

    Add Pagination

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -18,67 +18,69 @@ namespace api.Repository
 
         public async Task<Stock?> DeleteAsync(int id)
         {
-            var stockModel = await context.Stocks.FirstOrDefaultAsync(s=>s.Id==id);
-            
-            if(stockModel is null)
+            var stockModel = await context.Stocks.FirstOrDefaultAsync(s => s.Id == id);
+
+            if (stockModel is null)
                 return null;
-            
+
             context.Stocks.Remove(stockModel);
             await context.SaveChangesAsync();
-            
+
             return stockModel;
         }
 
         public async Task<List<Stock>> GetAllAsync(QueryObject queryObject)
         {
 
-          var stocks = context.Stocks.Include(c=>c.Comments).AsQueryable();
+            var stocks = context.Stocks.Include(c => c.Comments).AsQueryable();
 
-          if (!string.IsNullOrEmpty(queryObject.Symbol))
-          {
-            stocks = stocks.Where(s => s.Symbol.Contains(queryObject.Symbol));
-          }
-
-          if (!string.IsNullOrEmpty(queryObject.CompanyName))
-          {
-            stocks = stocks.Where(s => s.CompanyName.Contains(queryObject.CompanyName));
-          }
-
-          if(!string.IsNullOrEmpty(queryObject.SortBy))
-          {
-            if(queryObject.SortBy.ToLower() == "symbol")
+            if (!string.IsNullOrEmpty(queryObject.Symbol))
             {
-                stocks = queryObject.IsDecsending ? stocks.OrderByDescending(s => s.Symbol) : stocks.OrderBy(s => s.Symbol);
+                stocks = stocks.Where(s => s.Symbol.Contains(queryObject.Symbol));
             }
-            else if(queryObject.SortBy.ToLower() == "companyname")
-            {
-                stocks = queryObject.IsDecsending ? stocks.OrderByDescending(s => s.CompanyName) : stocks.OrderBy(s => s.CompanyName);
-            }
-          }
 
-          return await stocks.ToListAsync();
+            if (!string.IsNullOrEmpty(queryObject.CompanyName))
+            {
+                stocks = stocks.Where(s => s.CompanyName.Contains(queryObject.CompanyName));
+            }
+
+            if (!string.IsNullOrEmpty(queryObject.SortBy))
+            {
+                if (queryObject.SortBy.ToLower() == "symbol")
+                {
+                    stocks = queryObject.IsDecsending ? stocks.OrderByDescending(s => s.Symbol) : stocks.OrderBy(s => s.Symbol);
+                }
+                else if (queryObject.SortBy.ToLower() == "companyname")
+                {
+                    stocks = queryObject.IsDecsending ? stocks.OrderByDescending(s => s.CompanyName) : stocks.OrderBy(s => s.CompanyName);
+                }
+            }
+
+            var skipNumber = (queryObject.PageNumber - 1) * queryObject.PageSize;
+
+            return await stocks.Skip(skipNumber).Take(queryObject.PageSize).ToListAsync();
         }
 
         public async Task<Stock?> GetByIdAsync(int id)
         {
-            return await context.Stocks.Include(c=>c.Comments).FirstOrDefaultAsync(s=>s.Id==id);
+            return await context.Stocks.Include(c => c.Comments).FirstOrDefaultAsync(s => s.Id == id);
         }
 
         public Task<bool> IsExist(int id)
         {
-            return context.Stocks.AnyAsync(s=>s.Id==id);
+            return context.Stocks.AnyAsync(s => s.Id == id);
         }
 
         public async Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockRequestDto)
         {
-           var stockModel = await context.Stocks.FirstOrDefaultAsync(s=>s.Id==id);
-        
-            if(stockModel is null)
+            var stockModel = await context.Stocks.FirstOrDefaultAsync(s => s.Id == id);
+
+            if (stockModel is null)
                 return null;
 
             context.Entry(stockModel).CurrentValues.SetValues(stockRequestDto);
             await context.SaveChangesAsync();
-            
+
             return stockModel;
         }
     }
