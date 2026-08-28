21d52ae12e0b5e9201d53841d706e03cc32e1b40
Seif Mohamed <saif.eissa7797@gmail.com>
2026-08-28 19:23:47 +0300

    Add Register Endpoint

--- api/Data/ApplicationDbContext.cs
+++ api/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,5 +14,30 @@ namespace api.Data
 
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = "11111111-1111-1111-1111-111111111111",
+                    Name = "Admin",
+                    NormalizedName = "ADMIN",
+                    ConcurrencyStamp = "6fb7c1fa-bb73-4842-83f0-362fb4031a3e"
+                },
+                new IdentityRole
+                {
+                    Id = "22222222-2222-2222-2222-222222222222",
+                    Name = "User",
+                    NormalizedName = "USER",
+                    ConcurrencyStamp = "485b52ae-d01a-4471-8fb3-11ec28b0079d"
+                },
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
