0b9869b6bafef082ac96c81812fb5212312ab0f9
Seif Mohamed <saif.eissa7797@gmail.com>
2026-09-03 17:11:04 +0300

    Fetch Comments

--- frontend/src/Services/CommentService.ts
+++ frontend/src/Services/CommentService.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import type { CommentPost } from "../Models/Comment";
+import type { CommentGet, CommentPost } from "../Models/Comment";
 import { handleError } from "../Helpers/ErrorHandler";
 
 const api = "http://localhost:5000/api/comment/";
@@ -16,6 +16,15 @@ export const commentPostAPI = async (
       title: title,
       content: content,
     });
+    return data;
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const commentGetAPI = async (symbol: string) => {
+  try {
+    const data = await axios.get<CommentGet[]>(api + `?Symbol=${symbol}`);
     return data;
   } catch (error) {
     handleError(error);
